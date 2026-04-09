c76fcf414405c7bf5e74255f6ae55abfd3af6bb1
LauHuiQi0920 <vikii092028@gmail.com>
2026-04-10 00:40:24 +0800

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,177 @@
+"use client";
+
+import { Tangerine , Bilbo_Swash_Caps, UnifrakturMaguntia, EB_Garamond} from "next/font/google";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+
+const tangerine = Tangerine({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+  });
+
+const bilbo = Bilbo_Swash_Caps({
+    weight: ["400", "400"],
+    subsets: ["latin"],
+  });
+
+const unifraktur_magunitia = UnifrakturMaguntia({
+  weight: ["400", "400"],
+  subsets: ["latin"],
+});
+
+const eb_garamond = EB_Garamond({
+  weight: ["400", "400"],
+  subsets: ["latin"],
+});
+
+
 
 export default function Home() {
+  const [studentId, setStudentId] = useState("");
+  const [history, setHistory] = useState([]);
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (!studentId) {
+      alert("Please enter a Dragon Student ID");
+      return;
+    }
+    router.push(`/results?studentId=${studentId}`);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial, sans-serif",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "350px",
+          zIndex: -1,
+          WebkitMaskImage:
+            "linear-gradient(to bottom, black 70%, transparent 100%)",
+          maskImage:
+            "linear-gradient(to bottom, black 70%, transparent 100%)",
+        }}
+      >
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/background_image_1.png"
+          alt="Background"
+          fill
+          style={{ objectFit: "cover" }}
         />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+
+      <Image
+        src="/DPSLE_logo_.png"
+        alt="DPSLE Logo"
+        width={0}
+        height={0}
+        sizes="240px"
+        style={{
+          height: "240px",
+          width: "auto",
+          marginBottom: "10px",
+        }}
+      />
+      {/* Logo / Title */}
+      <h1
+        className={unifraktur_magunitia.className}
+        style={{
+          fontSize: "4rem",
+          marginBottom: "10px",
+          color: "#926D42",
+        }}
+      >
+        DPSLE Foundation
+      </h1>
+      <h2 
+      className={unifraktur_magunitia.className}
+      style={{ fontSize: "2.4rem", marginBottom: "20px", color: "#926D42", fontWeight: "normal" }}>
+        Dragon Primary School Leaving Examination
+      </h2>
+
+
+      {/* Description */}
+      <p 
+      className={eb_garamond.className}
+      style={{  fontSize: "1.3rem", maxWidth: "90%", marginBottom: "40px", color: "#926D42" }}>
+        The Dragon Primary School Leaving Examination (DPSLE) is an annual global examination that is taken by candidates at the end of their final year of dragon primary school education.
+      </p>
+
+      <p 
+      className={eb_garamond.className}
+      style={{  fontSize: "1.3rem", maxWidth: "90%", marginBottom: "40px", color: "#926D42" }}>
+        Established as a benchmark of excellence, the Dragon PSLE has a long-standing history of shaping disciplined,
+        knowledgeable, and resilient dragon students. Over the years, it has evolved to focus not only on academic
+        achievement but also on character development and lifelong learning.
+      </p>
+
+      <p 
+      className={eb_garamond.className}
+      style={{ fontWeight: "bold", fontSize: "1.3rem", maxWidth: "90%", marginBottom: "40px", color: "#926D42" }}>
+        Search your DPSLE Score here.
+      </p>
+
+      {/* Search Section */}
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+        }}
+      >
+       <input
+        type="text"
+        placeholder="Enter Student ID"
+        value={studentId}
+        onChange={(e) => setStudentId(e.target.value)}
+        style={{
+          padding: "10px",
+          fontSize: "1rem",
+          borderRadius: "8px",
+          backgroundColor: "#5B3C13", // input box color
+          border: "0px",
+          color: "white", // text color for readability
+
+          // Outline (outer border effect)
+          outline: "3px solid #926D42",
+          outlineOffset: "2px", // creates space between border and outline
+          width: "250px",
+        }}
+      />
+
+        <button
+          onClick={handleSearch}
+          style={{
+            padding: "10px 20px",
+            fontSize: "1rem",
+            backgroundColor: "#926D42",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+          }}
+        >
+          Search
+        </button>
+      </div>
+    </main>
   );
 }
---
+++ src/app/results/page.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+type Student = {
+  name: string;
+  studentId: string;
+  score: {
+    English: number;
+    "Mother Tongue (Chinese)": number;
+    Mathematics: number;
+    Science: number;
+    "Social Studies": number;
+  };
+};
+
+export default function ResultPage() {
+  const searchParams = useSearchParams();
+  const studentId = searchParams.get("studentId");
+
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [student, setStudent] = useState<Student | null>(null);
+
+  const students: Student[] = [
+    {
+      name: "Marshmellow Tan Yu Kui",
+      studentId: "D2019883568I",
+      score: {
+        English: 89,
+        "Mother Tongue (Chinese)": 92,
+        Mathematics: 90,
+        Science: 91,
+        "Social Studies": 83,
+      },
+    },
+    {
+      name: "Mayonnaise Tan Xin Yu",
+      studentId: "D2020744972J",
+      score: {
+        English: 96,
+        "Mother Tongue (Chinese)": 90,
+        Mathematics: 97,
+        Science: 98,
+        "Social Studies": 89,
+      },
+    },
+  ];
+
+  useEffect(() => {
+    if (!studentId) return;
+
+    setLoading(true);
+
+    setTimeout(() => {
+      const found = students.find(
+        (s) => s.studentId.toLowerCase() === studentId.toLowerCase()
+      );
+
+      setStudent(found || null);
+      setLoading(false);
+    }, 1500);
+  }, [studentId]);
+
+  const calculateTotal = (score: Student["score"]) => {
+    return Object.values(score).reduce((acc, val) => acc + val, 0);
+  };
+
+  if (!studentId) {
+    return (
+      <main style={{ textAlign: "center", padding: "40px" }}>
+        <h2>No Student ID provided</h2>
+      </main>
+    );
+  }
+
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial, sans-serif",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ color: "#926D42", marginBottom: "20px" }}>
+        Student Result
+      </h1>
+
+      {/* Loading */}
+      {loading && <p style={{ color: "#926D42" }}>Loading...</p>}
+
+      {/* Result */}
+      {!loading && student && (
+         <div
+        style={{
+            width: "90%",
+            border: "40px solid transparent",
+            borderImage: "url('/fancy_border_left.svg') 100 stretch",
+            padding: "20px",
+        }}
+        >
+        <div
+          style={{
+            padding: "10px",
+            width: "100%",
+            color: "#5B3C13",
+          }}
+        >
+          {/* Student Info Box */}
+        <div
+        style={{
+            backgroundColor: "#5B3C13",
+            color: "white",
+            padding: "15px",
+            borderRadius: "10px",
+            outline: "3px solid #5B3C13",
+            outlineOffset: "3px",
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+            marginBottom: "20px",
+        }}
+        >
+            <p>{student.name}</p>
+            <p style={{fontSize: "1.2rem"}}>ID: {student.studentId}</p>
+        </div>
+
+        {/* Score Box */}
+        <div
+        style={{
+            backgroundColor: "#926D42",
+            color: "white",
+            padding: "15px",
+            borderRadius: "10px",
+            outline: "3px solid #926D42",
+            outlineOffset: "3px",
+            fontSize: "1rem",
+        }}
+        >
+        <p>English: {student.score.English}</p>
+        <p>Chinese: {student.score["Mother Tongue (Chinese)"]}</p>
+        <p>Mathematics: {student.score.Mathematics}</p>
+        <p>Science: {student.score.Science}</p>
+        <p>Social Studies: {student.score["Social Studies"]}</p>
+        </div>
+
+        <hr style={{ margin: "10px 0" }} />
+
+          <h3 style={{fontSize: "1.5rem"}}>Total: {calculateTotal(student.score)}</h3>
+        </div>
+        </div>
+      )}
+
+      {/* Not found */}
+      {!loading && !student && (
+        <p style={{ color: "#926D42", paddingBottom: '10px' }}>Student not found</p>
+      )}
+    {!loading && (
+        <button 
+        onClick={() => router.back()}
+        style={{
+            marginBottom: "20px",
+            padding: "10px 20px",
+            backgroundColor: "#926D42",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            fontWeight: "bold",
+        }}
+        >
+        Back
+    </button>
+    )}
+    
+    </main>
+  );
+}
